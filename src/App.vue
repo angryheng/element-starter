29d069c3bb89cbd896562edaa5d41aacc540cc9b
zxh <angryheng@gmail.com>
2019-03-11 11:13:55 +0800

    day02

--- src/App.vue
+++ src/App.vue
@@ -1,231 +1,191 @@
 <template>
 <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-        <el-button type="danger" @click="show = !show">Click Me</el-button>
-        <el-button type="info" @click="show2 = !show2">Click Me</el-button>
-        <div style="display: flex; margin-top: 20px; height: 100px;">
-            <transition name="el-zoom-in-top">
-                <div v-show="show" class="transition-box">.el-fade-in-linear</div>
-            </transition>
-            <transition name="el-fade-in">
-                <div v-show="show" class="transition-box">.el-fade-in</div>
-            </transition>
-            <el-collapse-transition>
-                <div v-show="show2">
-                    <div class="transition-box">el-collapse-transition</div>
-                </div>
-            </el-collapse-transition>
-        </div>
-        <el-row :gutter="20">
-            <el-col :span="6">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-        </el-row>
-        <el-row :gutter="20">
-            <el-col :span="16">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-            <el-col :span="8">
-                <div class="grid-content bg-purple-light"></div>
-            </el-col>
-        </el-row>
-        <el-row type="flex" class="row-bg" justify="space-around">
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark"></div>
-            </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark"></div>
-            </el-col>
-            <el-col :span="6">
-                <div class="grid-content bg-purple-dark"></div>
-            </el-col>
-        </el-row>
-        <el-row :gutter="10">
-            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
-                <div class="grid-content bg-purple-light"></div>
-            </el-col>
-            <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
-                <div class="grid-content bg-purple"></div>
-            </el-col>
-            <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1">
-                <div class="grid-content bg-purple-light"></div>
-            </el-col>
-        </el-row>
-        <el-container>
-            <el-header>Header</el-header>
-            <el-main>Main</el-main>
-            <el-footer>Footer</el-footer>
-        </el-container>
-    </div>
-    <div style="margin-top: 20px;">
-        <el-container>
-            <el-header>Header</el-header>
-            <el-container>
-                <el-aside>Aside</el-aside>
-                <el-main>Main</el-main>
-            </el-container>
-            <el-footer>Footer</el-footer>
-        </el-container>
-        <i class="el-icon-edit"></i>
-        <i class="el-icon-arrow-down"></i>
-    </div>
+  <img src="./assets/logo.png">
+  <div class="mt20">
+    <el-radio-group v-model="radio">
+      <el-radio label="3">选项1</el-radio>
+      <el-radio label="6">选项2</el-radio>
+      <el-radio label="9">选项3</el-radio>
+    </el-radio-group>
+  </div>
+  <div class="mt20">
+    <el-radio-group v-model="radio2" size="small">
+      <el-radio-button label="1">选项1</el-radio-button>
+      <el-radio-button label="2">选项2</el-radio-button>
+    </el-radio-group>
+  </div>
+  <div class="mt20">
+    <el-radio-group v-model="radio3" size="small">
+      <el-radio-button label="4">选项1</el-radio-button>
+      <el-radio-button label="5">选项2</el-radio-button>
+      <el-radio-button disabled label="6">选项2</el-radio-button>
+    </el-radio-group>
+  </div>
+  <div class="mt20">
+    <el-radio-group v-model="radio4" size="mini" :disabled="true">
+      <el-radio-button label="北京"></el-radio-button>
+      <el-radio-button label="上海"></el-radio-button>
+      <el-radio-button label="深圳"></el-radio-button>
+      <el-radio-button label="杭州"></el-radio-button>
+    </el-radio-group>
+  </div>
+  <div class="mt20">
+    <el-radio-group v-model="radio5" size="large">
+      <el-radio border label="zyc">最爱</el-radio>
+      <el-radio border label="xxl">最恨</el-radio>
+    </el-radio-group>
+  </div>
+  <div class="mt20">
+    <el-checkbox v-model="checked">复选项</el-checkbox>
+  </div>
+  <div class="mt20">
+    <el-checkbox disabled v-model="checked1">复选项1</el-checkbox>
+    <el-checkbox disabled v-model="checked2">复选项2</el-checkbox>
+  </div>
+  <div class="mt20">
+    <el-checkbox-group v-model="checkList">
+      <el-checkbox label="选项A"></el-checkbox>
+      <el-checkbox label="选项B"></el-checkbox>
+      <el-checkbox label="选项C"></el-checkbox>
+      <el-checkbox :disabled="true" label="禁用"></el-checkbox>
+      <el-checkbox :disabled="true" label="选中且禁用"></el-checkbox>
+    </el-checkbox-group>
+  </div>
+  <div class="mt20">
+    <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
     <div class="mt20">
-        <el-row>
-            <el-button>默认按钮</el-button>
-            <el-button type="primary">按钮</el-button>
-            <el-button type="info">按钮</el-button>
-            <el-button type="danger">按钮</el-button>
-            <el-button type="warning">按钮</el-button>
-            <el-button type="success">按钮</el-button>
-        </el-row>
-        <el-row>
-            <el-button plain="true">朴素按钮</el-button>
-            <el-button plain="true" type="primary">按钮</el-button>
-            <el-button plain="true" type="info">按钮</el-button>
-            <el-button plain="true" type="danger">按钮</el-button>
-            <el-button plain="true" type="warning">按钮</el-button>
-            <el-button plain="true" type="success">按钮</el-button>
-        </el-row>
-        <el-row>
-            <el-button round>圆角按钮</el-button>
-            <el-button round type="primary">按钮</el-button>
-            <el-button round type="info">按钮</el-button>
-            <el-button round type="danger">按钮</el-button>
-            <el-button round type="warning">按钮</el-button>
-            <el-button round type="success" disabled="true">按钮</el-button>
-        </el-row>
-        <el-row>
-            <el-button icon="el-icon-edit" circle type="primary"></el-button>
-        </el-row>
-        <el-row>
-            <el-button type="text">按钮</el-button>
-            <el-button type="text" disabled="true">按钮</el-button>
-        </el-row>
-        <el-button-group>
-            <el-button type="primary" class="el-icon-arrow-left">上一页</el-button>
-            <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
-        </el-button-group>
+      <el-checkbox-group v-model="cityChecked" @change="handleCheckedCitiesChange">
+        <el-checkbox v-for="(city, index) of cities" :label="city" :key="index">{{city}}</el-checkbox>
+      </el-checkbox-group>
     </div>
-    <div class="mt20">
-        <el-row>
-            <el-button type="primary" :loading="true">加载中</el-button>
-            <el-button size="large">按钮</el-button>
-        </el-row>
-    </div>
+  </div>
+  <div class="mt20">
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名">
+      </el-table-column>
+      <el-table-column prop="address" label="地址">
+      </el-table-column>
+    </el-table>
+  </div>
+  <div class="mt20">
+    <el-table :data="tableData" border max-height="450" style="width: 50%">
+      <el-table-column fixed prop="date" label="日期" width="150">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120">
+      </el-table-column>
+      <el-table-column prop="province" label="省份" width="120">
+      </el-table-column>
+      <el-table-column prop="city" label="市区" width="120">
+      </el-table-column>
+      <el-table-column prop="address" label="地址" width="300">
+      </el-table-column>
+      <el-table-column prop="zip" label="邮编" width="120">
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+  <div class="mt20">
+  </div>
 </div>
 </template>
 
 <script>
+const cityOptions = ['上海', '北京', '杭州', '深圳'];
 export default {
-    data() {
-        return {
-            show: false,
-            show2: false
-        }
+  data() {
+    return {
+      radio: "3",
+      radio2: "1",
+      radio3: "5",
+      radio4: "上海",
+      radio5: "zyc",
+      checked: true,
+      checked1: true,
+      checked2: false,
+      checkList: ["选中且禁用", "选项A"],
+      isIndeterminate: true,
+      checkAll: false,
+      cities: cityOptions,
+      cityChecked: ["杭州", "深圳"],
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }]
     }
+  },
+  methods: {
+    tableRowClassName({
+      row,
+      rowIndex
+    }) {
+      if (rowIndex % 2 === 0) {
+        // return "warning-row"
+      } else {
+        // return "success-row"
+      }
+    },
+    handleCheckAllChange(val) {
+      this.cityChecked = val ? this.cities : [];
+      this.isIndeterminate = false;
+    },
+    handleCheckedCitiesChange(val) {
+      let checkCount = val.length;
+      this.checkAll = checkCount === this.cities.length ? true : false;
+      this.isIndeterminate = checkCount === this.cities.length ? false : true;
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-    font-family: Helvetica, sans-serif;
-    text-align: center;
+  font-family: Helvetica, sans-serif;
+  text-align: center;
 }
+
 .mt20 {
-    margin-top: 20px;
-}
-.el-header,
-.el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
+  margin-top: 20px;
 }
 
-.el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
+.el-table .warning-row {
+  background-color: #E6A23C;
+  color: #fff;
 }
 
-.el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-}
-
-body>.el-container {
-    margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-}
-
-.transition-box {
-    margin-bottom: 10px;
-    width: 200px;
-    height: 100px;
-    border-radius: 4px;
-    background-color: #409EFF;
-    text-align: center;
-    color: #fff;
-    padding: 40px 20px;
-    box-sizing: border-box;
-    margin-right: 20px;
-}
-
-.el-row {
-    margin-bottom: 20px;
-
-    &:last-child {
-        margin-bottom: 0;
-    }
-}
-
-.el-col {
-    border-radius: 4px;
-}
-
-.bg-purple-dark {
-    background: #99a9bf;
-}
-
-.bg-purple {
-    background: #d3dce6;
-}
-
-.bg-purple-light {
-    background: #e5e9f2;
-}
-
-.grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-}
-
-.row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
+.el-table .success-row {
+  color: #fff;
+  background-color: #67C23A;
 }
 </style>
